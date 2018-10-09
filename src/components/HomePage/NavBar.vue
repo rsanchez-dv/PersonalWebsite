25238f6aa250d6051370b620a91531c73d608796
Roberto Sanchez <rsanchez92@live.com>
2018-10-08 18:14:37 -0700

    Revamp

--- src/components/HomePage/NavBar.vue
+++ src/components/HomePage/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
-<nav class="navbar navbar-dark bg-dark" id="NavBarBackgroundColor">
-  <a class="navbar-brand" id="TitleName" href="#">Roberto Sanchez</a>
+<!-- Image and text -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Roberto Sanchez</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
