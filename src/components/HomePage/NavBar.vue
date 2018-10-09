446594c933115bf1e86112e03cabef6e3ab468ab
Roberto Sanchez <rsanchez92@live.com>
2018-10-08 19:48:40 -0700

    Moved Items to the right on nav bar

--- src/components/HomePage/NavBar.vue
+++ src/components/HomePage/NavBar.vue
@@ -1,16 +1,16 @@
 <template>
 <!-- Image and text -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">Roberto Sanchez</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
+  <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto">
       <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
+      <a class="nav-item nav-link" href="#">Projects / Experiences</a>
+      <a class="nav-item nav-link" href="#">Resume</a>
+      <a class="nav-item nav-link disabled" href="#">Contact Me</a>
     </div>
   </div>
 </nav>
