27fd6bdd95c6062610379186a020e2c7a4f7b4a4
Roberto Sanchez <rsanchez92@live.com>
2018-10-11 18:02:29 -0700

    Base Site
    Need more work though

--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
     <div class="jumbotron jumbotron-fluid">
-
       <video autoplay muted loop poster="https://dummyimage.com/900x400/000/fff">
         <source src="../../assets/background/Space.webm" data-src="" type="video/webm">
-
       </video>
-
       <div class="container text-white">
-
         <h1 class="display-4">Hello, world!</h1>
         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
           featured content or information.</p>
         <hr class="my-4">
         <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-
       </div>
       <!-- /.container -->
     </div>
@@ -24,6 +18,8 @@
       <div>
         <h1>About Me:</h1>
         <hr>
+        <div class="row">
+        <div class="col-8">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum laoreet dignissim. Vivamus lacinia
           ligula non mauris facilisis tempor. Mauris metus diam, accumsan efficitur augue nec, facilisis volutpat nisl.
           Donec vestibulum nibh sit amet ipsum vestibulum, sit amet consectetur massa tempor. Aliquam consequat vel sem
@@ -34,6 +30,11 @@
           id quam. Etiam rutrum libero turpis, feugiat consectetur dui pharetra et. Aliquam dictum, mi id malesuada
           luctus, quam ex cursus quam, vel molestie enim quam dignissim urna. Integer posuere, ipsum vitae accumsan
           pulvinar, neque quam consequat ligula, vel mollis risus eros aliquam ante.!</p>
+        </div>
+        <div class="col-4">
+          <img src="../../assets/profile.gif">
+        </div>
+      </div>
         <p>Vestibulum sollicitudin hendrerit eros, sed egestas eros laoreet non. Quisque aliquet diam nulla, eu
           tristique ipsum congue congue. Donec dictum elementum leo. Nam mattis velit ut justo pharetra scelerisque. In
           sit amet velit ipsum. Pellentesque sed sem tincidunt lectus lobortis facilisis. Sed tempor, diam a sodales
@@ -71,6 +72,65 @@
       <div>
         <h1>Skills:</h1>
         <hr>
+        <div class="row">
+          <div class="col-4"><i id="skillIcon" class="fas fa-desktop"></i>Programming Skills</div>
+          <div class="col-4">
+            <ul>
+              <li>C#</li>
+              <li>C++</li>
+              <li>Python</li>
+              <li>Java</li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>PHP</li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-4"><i id="skillIcon" class="fas fa-users"></i>Inter-Personal Skills</div>
+          <div class="col-4">
+            <ul>
+              <li>Verbal Communication</li>
+              <li>Constructive Feedback</li>
+              <li>Collaboration</li>
+              <li>Commitment</li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <ul>
+              <li>Self-Management</li>
+              <li>Optimism</li>
+              <li>Descision Making</li>
+              <li>Experimenting</li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+        <div class="row">
+          <div class="col-4"><i id="skillIcon" class="fas fa-question"></i>IDK something... technology</div>
+          <div class="col-4">
+            <ul>
+              <li>Linux</li>
+              <li>Windows</li>
+              <li>Raspbery</li>
+              <li>C#</li>
+            </ul>
+          </div>
+          <div class="col-4">
+            <ul>
+              <li>asdfsdf</li>
+              <li>asdfsdf</li>
+              <li>asdfsdf</li>
+              <li>asdfsdf</li>
+            </ul>
+          </div>
+        </div>
       </div>
 
       <div>
@@ -96,22 +156,77 @@
       <div>
         <h1>Projects:</h1>
         <hr>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum laoreet dignissim. Vivamus lacinia
-          ligula non mauris facilisis tempor. Mauris metus diam, accumsan efficitur augue nec, facilisis volutpat nisl.
-          Donec vestibulum nibh sit amet ipsum vestibulum, sit amet consectetur massa tempor. Aliquam consequat vel sem
-          quis sagittis. Duis dictum, tortor vel rhoncus iaculis, urna odio dictum augue, in suscipit lacus tortor sit
-          amet leo. Duis sodales interdum est, eget congue nunc ultrices et. Quisque non hendrerit arcu. Etiam congue
-          augue ac hendrerit molestie. Aliquam vel odio suscipit, lacinia purus a, pulvinar dui. Integer eleifend
-          elementum pulvinar. Morbi finibus, odio in congue rhoncus, mauris orci placerat sem, eu vehicula lorem magna
-          id quam. Etiam rutrum libero turpis, feugiat consectetur dui pharetra et. Aliquam dictum, mi id malesuada
-          luctus, quam ex cursus quam, vel molestie enim quam dignissim urna. Integer posuere, ipsum vitae accumsan
-          pulvinar, neque quam consequat ligula, vel mollis risus eros aliquam ante.!</p>
-        <p>Vestibulum sollicitudin hendrerit eros, sed egestas eros laoreet non. Quisque aliquet diam nulla, eu
-          tristique ipsum congue congue. Donec dictum elementum leo. Nam mattis velit ut justo pharetra scelerisque. In
-          sit amet velit ipsum. Pellentesque sed sem tincidunt lectus lobortis facilisis. Sed tempor, diam a sodales
-          rutrum, nunc lectus varius elit, a vestibulum augue augue non metus. Nam eleifend eu elit ac semper.
-          Vestibulum mollis, ligula quis consectetur lobortis, erat enim facilisis magna, eu tempor tortor neque non
-          mauris. Vestibulum posuere fermentum metus, eget congue purus viverra id. Donec id elementum lacus.</p>
+        <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+<div class="row">
+  <div class="col-6 col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="../../assets/profile.gif" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+  <div class="col-6 col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="../../assets/profile.gif" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+  <div class="col-6 col-md-4">
+    <div class="col-6 col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="../../assets/profile.gif" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+  </div>
+</div>
+<br>
+<div class="row">
+  <div class="col-6 col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="../../assets/profile.gif" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+  <div class="col-6 col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="../../assets/profile.gif" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+  <div class="col-6 col-md-4">
+    <div class="col-6 col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="../../assets/profile.gif" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+  </div>
+</div>
+<br>
       </div>
     </div>
     <Footer></Footer>
@@ -127,38 +242,38 @@ export default {
 }
 /* eslint-disable */
 function deferVideo () {
-  // defer html5 video loading
   $('video source').each(function () {
     var sourceFile = $(this).attr('data-src')
     $(this).attr('src', sourceFile)
     var video = this.parentElement
     video.load()
-  // uncomment if video is not autoplay
-  // video.play();
   })
 }
 window.onload = deferVideo
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .jumbotron {
   position: relative;
   overflow: hidden;
   background-color:black;
+  
 }
 .jumbotron video {
+  height: 25%;
   position: absolute;
   z-index: 1;
   top: 0;
   width:100%;
   height:100%;
-  /*  object-fit is not supported on IE  */
   object-fit: cover;
   opacity:0.5;
 }
 .jumbotron .container {
   z-index: 2;
   position: relative;
+}
+#skillIcon{
+ padding-right:10px; 
 }
 </style>
