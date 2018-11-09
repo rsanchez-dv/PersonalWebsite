4bb844b87c32c9b761e0a2db250638ffffa3c7cc
Roberto Sanchez <rsanchez92@live.com>
2018-11-09 12:18:53 -0800

    Fixed Typo

--- src/components/Home/Content.vue
+++ src/components/Home/Content.vue
@@ -12,7 +12,7 @@
           </b-col>
           <b-col sm="7">
             <b-row>
-              <p>I'm a recent graduate from California State University, Long Beach seeking an entry level position in either software engineering or web development. I've been programming since 2012 in C++ and Java then picking up web development during my later university life. I generally consider myself enthusiastic about technology since I was a kid I liked to tinker and see what is capable on computers. My interests are in the realm of web development, robotics, and automation.
+              <p>I'm a recent graduate from California State University, Long Beach seeking an entry level position in either software engineering or web development. I've been programming since 2012 in C++ and Java then picking up web development during my later university life. I generally consider myself enthusiastic about technology. Since I was a kid I liked to tinker and see what is capable on computers. My interests are in the realm of web development, robotics, and automation.
               </p>
             </b-row>
             <b-row>
